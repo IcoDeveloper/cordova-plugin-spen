fe6be75bf25411fbc83a6bb9ecb7a40efb0b285b
Jean-Maxime Couillard <jeanmaximecouillard@gmail.com>
2014-05-20 17:20:53 -0400

    Removed unused code

--- www/SpenPlugin.js
+++ www/SpenPlugin.js
@@ -1,43 +1,8 @@
-//****************************************************************************************
-//Author Name       :   Marcus Manvinder
-//Date              :   Sept-25-2012
-//Purpose           :   This Java script file provides an interface to interact from HTML to Canvas.
-//Table referred    :   NA
-//Table updated     :   NA
-//Most Important Related Files: org.apache.cordova.plugin.SpenPlugin.Java
-//****************************************************************************************
-
+// Javascript file imported automatically by cordova module loader (when isntalled correctly)
 
 var SpenPlugin = function() {
     //alert("Init SCanevas");
 };
-
-//********************************************************************************************************************
-// Function Name    :   showCanvas
-// Author Name      :   Marcus Manvinder
-// Date             :   Sept-25-2012
-// Input Parameters :   successCallback - The the success callback function.
-//                      errorCallback - The error callback function.
-// Purpose          :   The overridden function showCanvas.
-// ********************************************************************************************************************
-SpenPlugin.prototype.showCanvas = function(successCallback, errorCallback) {
-
-    if (errorCallback == null) { errorCallback = function() {}}
-   
-    if (typeof errorCallback != "function")  {
-        console.log("SpenPlugin.showCanvas failure: failure parameter not a function");
-        return
-    }
-	if (successCallback == null) { successCallback = function() {}}
-	
-    if (typeof successCallback != "function") {
-        console.log("SpenPlugin.showCanvas failure: success callback parameter must be a function");
-        return
-    }
-
-    return cordova.exec(successCallback, errorCallback, 'SpenPlugin', 'showCanvas', []);
-};
-
 
 SpenPlugin.prototype.addEvents = function(successCallback, errorCallback) {
 
@@ -58,4 +23,3 @@ SpenPlugin.prototype.addEvents = function(successCallback, errorCallback) {
 };
 
 module.exports = new SpenPlugin();
-
