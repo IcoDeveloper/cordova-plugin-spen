0d4036c8889e40909fbe44de321881a4c84e02ce
danturi <danturi89@gmail.com>
2014-12-28 19:28:24 +0100

    add module export

--- src/android/SpenPlugin.java
+++ src/android/SpenPlugin.java
@@ -1,4 +1,4 @@
-package it.polimi.spen;
+package org.apache.cordova.plugin;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
--- www/SpenPlugin.js
+++ www/SpenPlugin.js
@@ -1,8 +1,16 @@
-var SpenPlugin = {
-	addEvents : function() {
-		cordova.exec(function(param) {
-		}, function(error) {
-			alert("Unable to initialize pen support: " + error);
-		}, "SpenPlugin", "penEvents", []);
-	}
-};
+cordova.define("org.apache.cordova.plugin.SpenPlugin.SpenPlugin", function(
+		require, exports, module) {
+	var SpenPlugin = function() {
+		cordova.exec.setNativeToJsBridgeMode(cordova.exec.nativeToJsModes.PRIVATE_API);
+	};
+	
+		SpenPlugin.prototype.addEvents = function() {
+			cordova.exec(function(param) {
+			}, function(error) {
+				alert("Unable to initialize pen support: " + error);
+			}, "SpenPlugin", "penEvents", []);
+		}
+	
+	module.exports = new SpenPlugin();
+
+});
